8b1fc33bc30234f710f1183d0dcb546985ec15e4
AmyE123 <amy.elliott2002@yahoo.co.uk>
2022-07-09 17:05:20 +0100

    - Added basic fall & win detection (with debug logs for now)
    - Got rid of unused direction variable

--- Project/Player.gd
+++ Project/Player.gd
@@ -9,9 +9,16 @@ export var number_of_spaces = 2
 var velocity = Vector3.ZERO
 
 func _physics_process(delta):
-	var direction = Vector3.ZERO
 	velocity.y -= fall_acceleration * delta
 	velocity = move_and_slide(velocity, Vector3.UP)
+	
+	if(velocity.y <= -10):
+		print("Fall")
+		
+	for index in get_slide_count():
+		var collision = get_slide_collision(index)
+		if(collision.collider.is_in_group("win_block")):
+			print("Collided")
 
 func _input(event):
 	if(is_moving == false):
